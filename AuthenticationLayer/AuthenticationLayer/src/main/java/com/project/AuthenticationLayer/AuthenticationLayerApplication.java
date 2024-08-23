0d7d239329ef33734014d49d77b4f1c56fb856cc
Nitin <nitinbhalla15@gmail.com>
2024-08-24 00:15:16 +0530

    initial commit

---
+++ AuthenticationLayer/AuthenticationLayer/src/main/java/com/project/AuthenticationLayer/AuthenticationLayerApplication.java
@@ -0,0 +1,13 @@
+package com.project.AuthenticationLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationLayerApplication.class, args);
+	}
+
+}
